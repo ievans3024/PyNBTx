ad3ed9fce2d875da0048a61c4e3f000bf31b0aa0
ievans3024 <ievans3024@gmail.com>
2014-04-11 13:39:10 -0700

    + added icon definitions + created complete toolbar

--- __init__.py
+++ __init__.py
@@ -4,15 +4,88 @@
 __author__ = 'ievans3024'
 __version__ = '0.0.1'
 
-from tkinter import Tk, BOTH, PhotoImage
-from tkinter.ttk import Frame, Style, Button
+from tkinter import Tk, BOTH, N, PhotoImage, S
+from tkinter.ttk import Button, Frame, Separator, Style
 
-# This has to exist for references in class definitions
-icons = {}
+# Tk instantiated outside of __main__ to allow use of Image classes for icon definitions
+root = Tk()
+
+# Icon definitions are necessary because widgets do not save data provided to the 'image' kwarg to the instance.
+# To prevent image data from being garbage collected, they must exist in an external possible reference.
+icons = {
+    'tags': {
+        'byte': PhotoImage(file='icons/tag-byte.png'),
+        'byte_array': PhotoImage(file='icons/tag-byte-array.png'),
+        'compound': PhotoImage(file='icons/tag-compound.png'),
+        'double': PhotoImage(file='icons/tag-double.png'),
+        'float': PhotoImage(file='icons/tag-float.png'),
+        'int': PhotoImage(file='icons/tag-int.png'),
+        'int_array': PhotoImage(file='icons/tag-int-array.png'),
+        'list': PhotoImage(file='icons/tag-list.png'),
+        'long': PhotoImage(file='icons/tag-long.png'),
+        'short': PhotoImage(file='icons/tag-short.png'),
+        'string': PhotoImage(file='icons/text-plain.png'),
+    },
+    'actions': {
+        'delete': PhotoImage(file='icons/list-remove.png'),
+        'edit': PhotoImage(file='icons/gtk-edit.png'),
+        'new': {
+            'tag': {
+                'byte': PhotoImage(file='icons/tag-byte-new.png'),
+                'byte_array': PhotoImage(file='icons/tag-byte-array-new.png'),
+                'compound': PhotoImage(file='icons/tag-compound-new.png'),
+                'double': PhotoImage(file='icons/tag-double-new.png'),
+                'float': PhotoImage(file='icons/tag-float-new.png'),
+                'int': PhotoImage(file='icons/tag-int-new.png'),
+                'int_array': PhotoImage(file='icons/tag-int-array-new.png'),
+                'list': PhotoImage(file='icons/tag-list-new.png'),
+                'long': PhotoImage(file='icons/tag-long-new.png'),
+                'short': PhotoImage(file='icons/tag-short-new.png'),
+                'string': PhotoImage(file='icons/tag-string-new.png'),
+            }
+        },
+        'open': PhotoImage(file='icons/document-open.png'),
+        'save': PhotoImage(file='icons/media-floppy.png'),
+        'search': PhotoImage(file='icons/edit-find.png')
+    }
+}
 
 
 class ToolBar(Frame):
-    pass
+
+    def __init__(self, parent):
+
+        Frame.__init__(self, parent)
+
+        self.parent = parent
+        self.pack(padx=3)
+
+        elements = [
+            Button(self, image=icons['actions']['open']),
+            Button(self, image=icons['actions']['save']),
+            Separator(self, orient='vertical'),
+            Button(self, image=icons['actions']['search']),
+            Button(self, image=icons['actions']['edit']),
+            Button(self, image=icons['actions']['delete']),
+            Separator(self, orient='vertical'),
+            Button(self, image=icons['actions']['new']['tag']['byte']),
+            Button(self, image=icons['actions']['new']['tag']['short']),
+            Button(self, image=icons['actions']['new']['tag']['int']),
+            Button(self, image=icons['actions']['new']['tag']['long']),
+            Button(self, image=icons['actions']['new']['tag']['float']),
+            Button(self, image=icons['actions']['new']['tag']['double']),
+            Button(self, image=icons['actions']['new']['tag']['byte_array']),
+            Button(self, image=icons['actions']['new']['tag']['int_array']),
+            Button(self, image=icons['actions']['new']['tag']['string']),
+            Button(self, image=icons['actions']['new']['tag']['list']),
+            Button(self, image=icons['actions']['new']['tag']['compound'])
+        ]
+
+        for e in elements:
+            if isinstance(e, Separator):
+                e.grid(row=0, column=elements.index(e), sticky=N+S, padx=6)
+            else:
+                e.grid(row=0, column=elements.index(e), padx=1, pady=4)
 
 
 class TreeDisplay(Frame):
@@ -30,11 +103,12 @@ class MainWindow(Frame):
 
         self.style = Style()
         self.style.theme_use('default')
+        self.style.configure('TButton', padding=0)
 
         self.pack(fill=BOTH, expand=1)
 
-        button = Button(self, image=icons['TAG']['byte'])
-        button.pack()
+        toolbar = ToolBar(self)
+        toolbar.pack()
 
     def ui_init(self):
         pass
@@ -42,15 +116,6 @@ class MainWindow(Frame):
 
 def main():
 
-    root = Tk()
-
-    # TODO: Move this outside of main() or __main__ requirement
-    icons['TAG'] = {
-        'byte': PhotoImage(file='icons/tag-byte.png'),
-        'new': {
-
-        }
-    }
     app = MainWindow(root)
     root.mainloop()
 
